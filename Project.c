1919675f70bac574ea2249a069fe57913438bd11
Hamed Alinezhad <halinezhad325@gmail.com>
2025-01-01 14:42:43 +0330

    music add (1)

--- Project.c
+++ Project.c
@@ -2,12 +2,24 @@
 #include<ncurses.h>
 #include<string.h>
 #include<stdlib.h>
-
+#include <SDL2/SDL.h>
+#include <pthread.h>
+#include <SDL2/SDL_mixer.h>
+typedef struct Player{
+    char name[50];
+    int age;
+    int score;
+} Player;
+int comparePlayers(const void *a, const void *b);
 int loading_screen();
 int main_menu(int height, int width);
-int handle_input(char ch);
+int handle_input(int height, int width);
 int OPTIONS(int height, int width);
-
+int MUSIC_SELECTION(int height, int width);
+void init_audio();
+void play_music(const char *file);
+void stop_music();
+int SCORE_BOARD(int height, int width);
 
 
 int main(){
@@ -21,40 +33,7 @@ int main(){
     start_color();
     bkgd(COLOR_BLACK);
     loading_screen();
-    
-    while (1) {
-        int selected_option = main_menu(height, width);
-
-        switch (selected_option) {
-            case 1:
-                mvprintw(1, 1, "Start Game Selected");
-                refresh();
-                getch();
-                break;
-            case 2:
-                while (1) {
-                    int selected = OPTIONS(height, width);
-                    if (selected == 3) {
-                        break;      
-                    }
-                }
-                break;
-            case 3:
-                SCORE_BOARD();
-                return 0;
-            case 4:
-                clear();
-                mvprintw(height / 2, width / 2 - 4, "EXITING...");
-                refresh();
-                napms(3000); 
-                endwin();   
-                return 0; 
-            default:
-                break;
-        }
-    }
-
-
+    handle_input(height, width);
     // Clear screen and display the result
     
     // while ((ch = getchar()) != 27) {  // 27 is the ESC key
@@ -260,51 +239,222 @@ int main_menu(int ter_height, int ter_width) {
     }
     return 1;
 }
-int OPTIONS(int ter_height, int ter_width){
+int OPTIONS(int height, int width) {
     clear();
     refresh();
-
-    int height = ter_height * 2 / 5, width = ter_width * 2 / 5, starty = ter_height * 3 / 10, startx = ter_width * 3 / 10;
-    WINDOW *options_win = newwin(height, width, starty, startx);
-    const char *options_items[] = {"DIFFICULTY", "CHARACTER COLOR", "BACK"};
-    int n_items = sizeof(options_items) / sizeof(options_items[0]);
-    int choice = 0;
+    const char *options[] = {
+        "CHANGE MUSIC",
+        "CHARACTER COLOR",
+        "ADJUST DIFFICULTY",
+        "BACK"
+    };
+    int n_options = sizeof(options) / sizeof(options[0]);
+    int selected = 0;
     int key;
+    int current_music = 0; // Variable to store the selected music track
+
+    WINDOW *options_win = newwin(15, 40, (height - 15) / 2, (width - 40) / 2);
     box(options_win, 0, 0);
+
     while (1) {
-        werase(options_win);
-        box(options_win, 0, 0); 
-        for (int i = 0; i < n_items; i++) {
-            if (i == choice) {
+        mvwprintw(options_win, 1, 2, "OPTIONS");
+        for (int i = 0; i < n_options; i++) {
+            if (i == selected) {
                 wattron(options_win, A_REVERSE);
-                mvwprintw(options_win, 6 + 4 * i, (width - 12) / 2, "%s", options_items[i]);
+                mvwprintw(options_win, 5 + 2 * i, 2, "%s", options[i]);
                 wattroff(options_win, A_REVERSE);
             } else {
-                mvwprintw(options_win, 6 + 4 * i, (width - 12) / 2, "%s", options_items[i]);
+                mvwprintw(options_win, 5 + 2 * i, 2, "%s", options[i]);
             }
         }
         wrefresh(options_win);
-        key = getch();
 
+        key = getch();
         switch (key) {
             case KEY_UP:
-                choice--;
-                if (choice < 0)
-                    choice = n_items - 1;
+                selected--;
+                if (selected < 0) selected = n_options - 1;
                 break;
             case KEY_DOWN:
-                choice++;
-                if (choice >= n_items)
-                    choice = 0; 
+                selected++;
+                if (selected >= n_options) selected = 0;
                 break;
-            case '\n':
-                delwin(options_win); 
-                return choice + 1; 
+            case '\n': // ENTER key
+                if (selected == 0) { // Change Music
+                    current_music = MUSIC_SELECTION(height, width);
+                    if (current_music == 4){
+                        clear();
+                        refresh();
+                        break;
+                    }
+                    else if (current_music == 1){
+                        init_audio();
+                        play_music("c418_-_aria_math.mp3");
+                        SDL_Delay(20000);
+                        stop_music();
+                        clear();
+                        refresh();
+                    }
+                    else if (current_music != -1) {
+                        clear();
+                        refresh();
+                    }
+                } else if (selected == n_options - 1) { // Back
+                    delwin(options_win);
+                    return 1;
+                }
+                break;
         }
     }
-    return 1;
+}
+int MUSIC_SELECTION(int height, int width) {
+    clear();
+    refresh();
 
+    const char *music_tracks[] = {
+        "Track 1: Adventure",
+        "Track 2: Battle Theme",
+        "Track 3: Calm Forest",
+        "Exit"
+    };
+    int n_tracks = sizeof(music_tracks) / sizeof(music_tracks[0]);
+    int selected = 0;
+    int key;
+
+    WINDOW *music_win = newwin(10, 40, (height - 10) / 2, (width - 40) / 2);
+    box(music_win, 0, 0);
+
+    while (1) {
+        mvwprintw(music_win, 1, 2, "MUSIC SELECTION");
+        for (int i = 0; i < n_tracks; i++) {
+            if (i == selected) {
+                wattron(music_win, A_REVERSE);
+                mvwprintw(music_win, 3 + i, 2, "%s", music_tracks[i]);
+                wattroff(music_win, A_REVERSE);
+            } else {
+                mvwprintw(music_win, 3 + i, 2, "%s", music_tracks[i]);
+            }
+        }
+        wrefresh(music_win);
+
+        key = getch();
+        switch (key) {
+            case KEY_UP:
+                selected--;
+                if (selected < 0) selected = n_tracks - 1;
+                break;
+            case KEY_DOWN:
+                selected++;
+                if (selected >= n_tracks) selected = 0;
+                break;
+            case '\n': // ENTER key
+                return selected + 1; // Return the index of the selected track
+        }
+    }
 }
-int handle_input(char ch){
-    
+
+int SCORE_BOARD(int height, int width) {
+    clear();
+    refresh();
+
+    FILE *scoreB = fopen("SCOREBOARD.txt", "r");
+    if (!scoreB) {
+        mvprintw(height / 2, (width - 20) / 2, "Unable to open scoreboard file!");
+        refresh();
+        getch();
+        return 1;
+    }
+
+    Player players[100];
+    int player_count = 0;
+
+    while (fscanf(scoreB, "%s %d %d", players[player_count].name, 
+                  &players[player_count].age, &players[player_count].score) == 3) {
+        player_count++;
+        if (player_count >= 100) {
+            break;
+        }
+    }
+    fclose(scoreB);
+    qsort(players, player_count, sizeof(Player), comparePlayers);
+    WINDOW *score_win = newwin(20, 60, (height - 20) / 2, (width - 60) / 2);
+    box(score_win, 0, 0);
+    mvwprintw(score_win, 1, 2, "SCOREBOARD");
+    wrefresh(score_win);
+
+    int line = 2;
+    for (int i = 0; i < player_count && line < 18; i++) { // Prevent overflow
+        mvwprintw(score_win, ++line, 2, "%-10s %5d %5d", players[i].name, players[i].age, players[i].score);
+        wrefresh(score_win);
+    }
+
+    mvwprintw(score_win, line + 2, 2, "Press any key to return...");
+    wrefresh(score_win);
+
+    getch();
+    delwin(score_win);
+    return 1;
 }
+int comparePlayers(const void *a, const void *b) {
+    Player *playerA = (Player *)a;
+    Player *playerB = (Player *)b;
+    return playerB->score - playerA->score;
+}
+int handle_input(int height, int width){
+    while (1) {
+        int selected_option = main_menu(height, width);
+
+        switch (selected_option) {
+            case 1:
+                mvprintw(1, 1, "Start Game Selected");
+                refresh();
+                getch();
+                break;
+            case 2:
+                while (1) {
+                    int selected = OPTIONS(height, width);
+                    if (selected == 1) {
+                        break;      
+                    }
+                }
+                break;
+            case 3:
+                SCORE_BOARD(height, width);
+                break;
+            case 4:
+                clear();
+                mvprintw(height / 2, width / 2 - 4, "EXITING...");
+                refresh();
+                napms(300); 
+                endwin();   
+                return 0; 
+            default:
+                break;
+        }
+    }
+}
+void init_audio() {
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        printw("Failed to initialize SDL audio: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        printw("SDL_Mixer Error: %s\n", Mix_GetError());
+        exit(1);
+    }
+}
+void play_music(const char *file) {
+    Mix_Music *music = Mix_LoadMUS(file);
+    if (!music) {
+        printf("Failed to load music: %s\n", Mix_GetError());
+        return;
+    }
+
+    Mix_PlayMusic(music, -1); // Play the music in a loop
+}
+void stop_music() {
+    Mix_HaltMusic();
+    Mix_CloseAudio();
+}
+
